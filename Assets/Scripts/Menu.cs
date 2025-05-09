26f588729eae8116bf027a09b7c1e38b25ffba30
Alexander Summers <56906047+asummers1@users.noreply.github.com>
2025-05-09 19:03:15 -0400

    Update Menu.cs

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -5,6 +5,7 @@ using UnityEngine.Events;
 
 public class Menu : MonoBehaviour
 {
+    //Adapted from "Unity Game Development Cookbook", published by O'Reilly
     public UnityEvent menuDidAppear = new UnityEvent();
 
     public UnityEvent menuWillDisappear = new UnityEvent();
